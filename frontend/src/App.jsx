53acf25851dfef97eb48ddad5046359f0c7c0f54
Hans Veltman <hansveltman227@Hotmail.com>
2025-09-10 14:39:32 +0200

    Update App.jsx met startgrid

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,21 @@ import SudokuGrid from "./SudokuGrid";
 console.log(">>> Sudoku App.jsx is geladen <<<");
 
 function App() {
-  const [grid, setGrid] = useState(Array.from({ length: 9 }, () => Array(9).fill(0)));
+  // const [grid, setGrid] = useState(Array.from({ length: 9 }, () => Array(9).fill(0)));
+  const [grid, setGrid] = useState([
+  [5,3,0, 0,7,0, 0,0,0],
+  [6,0,0, 1,9,5, 0,0,0],
+  [0,9,8, 0,0,0, 0,6,0],
+
+  [8,0,0, 0,6,0, 0,0,3],
+  [4,0,0, 8,0,3, 0,0,1],
+  [7,0,0, 0,2,0, 0,0,6],
+
+  [0,6,0, 0,0,0, 2,8,0],
+  [0,0,0, 4,1,9, 0,0,5],
+  [0,0,0, 0,8,0, 0,7,9]
+]);
+
   const [solution, setSolution] = useState(null);
 
   const solve = async () => {
